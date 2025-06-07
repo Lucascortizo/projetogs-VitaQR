83a5f07409247c78eee3f149f30b4c72773d894a
Lucascortizo <lucascortizo768@gmail.com>
2025-06-06 21:32:47 -0300

    feat: adicionar footer

--- projetogs-vitaQR/src/App.jsx
+++ projetogs-vitaQR/src/App.jsx
@@ -3,18 +3,20 @@ import { HashRouter, Routes, Route } from "react-router-dom";
 import Home from "./routes/Home";
 import View from "./routes/View";
 import Nav from "./components/Nav";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <HashRouter>
-      <div className="min-h-screen bg-gray-50 text-gray-800">
+      <div className="min-h-screen bg-gray-50 text-gray-800 flex flex-col">
         <Nav />
-        <main className="p-4">
+        <main className="p-4 flex-1">
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/view" element={<View />} />
           </Routes>
         </main>
+        <Footer />
       </div>
     </HashRouter>
   );
---
+++ projetogs-vitaQR/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => (
+  <footer className="bg-white text-gray-700 py-4 mt-5 border-t">
+    <div className="container mx-auto text-center">
+      &copy; 2025 VitaQR. Todos os direitos reservados.
+    </div>
+  </footer>
+);
+
+export default Footer;
